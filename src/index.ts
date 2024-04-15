6582d7fc2a07d20a1d1678aa5c61d3970997c88e
afeiship <1290657123@qq.com>
2024-04-16 07:13:52 +0800

    feat: upt offset

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
 interface LastChartOptions {
   char?: string;
-  offset?: number;
+  offset?: { x: number; y: number };
   textElement?: any;
   textContainer?: any;
 }
 
 const defaults: LastChartOptions = {
   char: '.',
-  offset: -10,
+  offset: { x: 0, y: -10 },
   textElement: document.body,
   textContainer: document.body,
 };
 
 function getLastTextNode(node: any) {
-  if (node.nodeType === Node.TEXT_NODE) {
-    return node;
-  }
+  if (node.nodeType === Node.TEXT_NODE) return node;
   for (let i = node.childNodes.length - 1; i >= 0; i--) {
     let result = getLastTextNode(node.childNodes[i]);
     if (result) {
@@ -31,9 +29,9 @@ function getLastChar(inOptions: LastChartOptions) {
   const textContainer = options.textContainer;
   const offset = options.offset!;
 
-  // 获取最后一个文本元素
+  // Get last text node
   let lastTextNode = getLastTextNode(textElement);
-  // 创建一个临时的文本节点，以便选中最后一个文字
+  // Create a temp text node
   let tempText = document.createTextNode(options.char!);
   if (lastTextNode) {
     lastTextNode.parentNode.appendChild(tempText);
@@ -41,17 +39,17 @@ function getLastChar(inOptions: LastChartOptions) {
     textElement.appendChild(tempText);
   }
 
-  // 选中最后一个文字
+  // Select the temp text node
   const range = document.createRange();
   range.setStart(tempText, 0);
   range.setEnd(tempText, 0);
 
-  // 这个文字是相对于浏览器
+  // This is the position of the last character
   const rect = range.getBoundingClientRect();
   const textRect = textContainer.getBoundingClientRect();
 
-  const x = rect.left - textRect.left;
-  const y = rect.top - textRect.top + offset;
+  const x = rect.left - textRect.left + offset.x;
+  const y = rect.top - textRect.top + offset.y;
 
   tempText.remove();
 
